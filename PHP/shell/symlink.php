3a57e9f40a81dc208c898a7b000a8fa127ada464
Florian Rachor <f.rachor@websafari.eu>
2012-04-27 14:10:52 +0200

    Add php files for tar and symlink shell commands.

---
+++ PHP/shell/symlink.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Use this file to add symlinks without shell access.
+ * Will not work when exec is blocked for php
+ * Delete after use and ABSOLUTELY NO GET/POST INPUT!
+ *
+ * @author    Florian Rachor <f.rachor@websafari.eu>
+ */
+
+$linkName = '';
+$linkTarget = '';
+
+$return = symlink($linkTarget, $linkName);
+
+if($return) echo "Your link was successfully created";
+else echo "I'm really sorry, something has gone wrong";
---
+++ PHP/shell/tar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * If you need to tar / untar a file with no shell access.
+ * Will not work when exec is blocked for php
+ * Delete after use and ABSOLUTELY NO GET/POST INPUT!
+ *
+ * @author    Florian Rachor <f.rachor@websafari.eu>
+ */
+
+// Filename of the tar.gz archive
+$fileName = '';
+
+// For tar, directory that will be packed
+$directory = '';
+
+// Just empty if you want to tar something
+$cmd = 'untar';
+
+if($cmd == 'untar') {
+    $shellCmd = 'tar -xzvf ' . $fileName;
+} else $shellCmd = 'tar -cfvz ' . $fileName . ' ' . $directory;
+
+$shellCmd = escapeshellcmd($shellCmd);
+exec($shellCmd, $res);
+
+// Uncomment if you want verbose output
+//print_r($res);
