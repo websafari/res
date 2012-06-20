660d59aee46fc5e4917f9330070a43f3067f7974
Florian Rachor <f.rachor@websafari.eu>
2012-06-20 13:26:49 +0200

    Add more styles vor CSS

---
+++ TYPO3/Base/fileadmin/setup/ts/page.TSConfig.ts
@@ -0,0 +1,10 @@
+RTE.default.init {
+	theme_advanced_buttons1=newdocument, |, undo, redo, |, justifyleft, justifycenter, justifyright, justifyfull, |, pastetext, pasteword, |, fullscreen
+	theme_advanced_buttons2=link, typo3link, unlink, |, image, typo3image, |, table, |, row_props, cell_props, |, row_before, row_after, delete_row, |, col_before, col_after, delete_col, |, split_cells, merge_cells
+	theme_advanced_buttons3=code, |, anchor, charmap, |, forecolor, backcolor, strikethrough, sub, sup, |, bullist, numlist, |, outdent, indent, |, blockquote
+	theme_advanced_buttons4=styleselect, |, formatselect, |, fontsizeselect, |, bold, italic, underline
+	plugins=contextmenu, inlinepopups, safari, paste, fullscreen, advlink, typo3filemanager, advimage, table
+
+	content_css = fileadmin/setup/css/rte.css
+	theme_advanced_styles = Highlight Pink=highlightPink;Hightlight Blue=highlightBlue;
+}
